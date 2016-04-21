7185e91ac776968ece595b360b4759758f7db5ef
chenping06 <chenping06@baidu.com>
2016-04-21 15:08:27 +0800

    demo

--- demo/fis-conf.js
+++ demo/fis-conf.js
@@ -1,7 +1,26 @@
 
 fis.set('namespace', 'remreset');
-fis.match('*.less', {
-	parser: fis.plugin('less'),
-	rExt: '.css',
-	release: 'reset'
+
+// 编译less, px转换为rem
+fis.match("*.less", {
+    parser : fis.plugin('less'),
+    postprocessor: fis.plugin('remreset', {
+    	dpr: 2, // 2 = 视觉稿对应设备的dpr值
+    	remUnit: 75 // 75 = 视觉稿宽度/10
+    }),
+    rExt   : '.css'
+});
+
+// 发布
+fis.match('*', {
+	release: '/static/remreset/$0'
+})
+.match('{/node_modules/**,package.json}', {
+	release: false
+})
+.match('**', {
+    deploy: [
+        fis.plugin('encoding'),
+        fis.plugin('local-deliver')
+    ]
 });
